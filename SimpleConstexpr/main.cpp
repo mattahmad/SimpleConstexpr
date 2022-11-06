90d1f58b32b524328f470e9d93afdf6bf9776ce7
matt.a@myplanet.com <matt.a@myplanet.com>
2022-11-06 18:49:02 -0500

    Initial Commit

---
+++ SimpleConstexpr/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SimpleConstexpr
+//
+//  Created by Matt Ahmad on 2022-11-06.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
