b530b9bea0306610f18c80b2fad674f819721899
matt.a@myplanet.com <matt.a@myplanet.com>
2022-11-06 18:57:14 -0500

    Push simple constexpr

--- SimpleConstexpr/main.cpp
+++ SimpleConstexpr/main.cpp
@@ -1,14 +1,26 @@
-//
-//  main.cpp
-//  SimpleConstexpr
-//
-//  Created by Matt Ahmad on 2022-11-06.
-//
-
 #include <iostream>
 
+constexpr auto gcd(int a, int b){
+    while (b != 0){
+        auto t=b;
+        b= a % b;
+        a= t;
+    }
+    return a;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+    std::cout << "\n";
+    
+    constexpr int i= gcd(11, 121);
+    
+    int a= 11;
+    int b= 121;
+    int j= gcd(a, b);
+    
+    std::cout << "gcd(11,121): " << i << '\n';
+    std::cout << "gcd(a,b): " << j << '\n';
+    
+    std::cout << '\n';
 }
